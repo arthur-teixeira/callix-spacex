14932bdebf64a6feefab63f98675e42eb5d19633
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2023-03-09 20:49:09 -0300

    fix: added CORS configuration

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+
+import cors from 'cors';
 import express, { Express } from 'express';
 import routes from './src/routes';
 
@@ -8,6 +10,7 @@ import routes from './src/routes';
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(cors());
 app.use('/', routes);
 
 app.listen(port, () => {
