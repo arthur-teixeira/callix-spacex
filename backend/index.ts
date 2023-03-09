bf76d307429e87830f0a56e454f883de64346b35
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2023-03-08 21:15:49 -0300

    fix: load environment variables before imports

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express, { Express } from 'express';
 import routes from './src/routes';
 
-const app: Express = express();
-const port = 8000;
 
-app.use('/api', routes);
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use('/', routes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
