5934699f625c0f76636da24b6a5de79ad2b6d976
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2023-03-11 10:20:38 -0300

    feat: Added error handling middleware

--- backend/index.ts
+++ backend/index.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 
 import cors from 'cors';
-import express, { Express } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import routes from './src/routes';
 
 
@@ -12,6 +12,12 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use('/', routes);
+
+app.use((err: Error, _: Request, res: Response, __: NextFunction) => {
+    res.status(500).json({
+        message: `Um erro inesperado ocorreu: ${err.message}`,
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
